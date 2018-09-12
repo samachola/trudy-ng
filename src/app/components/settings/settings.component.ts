59ff1aff3bf79dd36c920013646f7ce5d954079b
Sam Achola <sam.achola@live.com>
2018-09-12 11:38:55 +0300

    (ft-settings) Implement google places autocomplete.

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { CategoriesService } from '../../services/categories/categories.service';
+import { MapsAPILoader } from '@agm/core';
+import {} from '@types/googlemaps';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  lat: number;
+  lng: number;
+  categories: any;
 
+  @ViewChild('search' ) public searchElement: ElementRef;
+  constructor(
+    private categoriesService: CategoriesService,
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+    ) { }
+
+  user: any;
   ngOnInit() {
+    const currentUser = localStorage.getItem('user');
+    this.user = JSON.parse(currentUser);
+    console.log(this.user);
+
+    this.getCategories();
+
+    this.mapsAPILoader.load().then(
+      () => {
+        const autocomplete = new google.maps.places.Autocomplete(this.searchElement.nativeElement, { types: ['address']});
+        autocomplete.addListener('place_changed', () => {
+          this.ngZone.run(() => {
+            const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+            console.log(JSON.stringify(place.geometry.location));
+            this.lat = place.geometry.location.lat();
+            this.lng = place.geometry.location.lng();
+
+            if (place.geometry === undefined || place.geometry === null) {
+              return;
+            }
+          });
+        });
+      }
+    );
+  }
+
+  /**
+   * Get all Categories.
+   *
+   * @return {Array} - List of categories.
+   */
+  getCategories() {
+    this.categoriesService.getAllCategories()
+                          .toPromise()
+                          .then(res => this.categories = res)
+                          .catch(err => console.log(err));
   }
 
 }
